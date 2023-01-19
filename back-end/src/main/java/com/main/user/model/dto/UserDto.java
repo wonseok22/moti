629ef94b4f04d9c67028d685c6cb5db2a6287283
YU DAVID <loveneverfails95@naver.com>
2023-01-19 16:56:39 +0900

    master 브랜치 파일 넣고 다시 커밋

---
+++ back-end/src/main/java/com/main/user/model/dto/UserDto.java
@@ -0,0 +1,27 @@
+package com.main.user.model.dto;
+
+import com.main.user.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private int userNo;
+    private String userId;
+    private String password;
+    private String email;
+    private String nickname;
+    private String type;
+    private int temporary;
+    private String joinDate;
+
+    public User toEntity() {
+        return new User(userNo, userId, password, email, nickname, null, type,null,  temporary, joinDate);
+    }
+}

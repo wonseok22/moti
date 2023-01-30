04b5239e16b2a8492cfc2352093ed0543a51cdc3
LEEYEONSOO <noprob1325@naver.com>
2023-01-30 15:29:10 +0900

    Comment 삭제 구현

--- src/main/java/com/main/feed/model/dto/CommentDto.java
+++ src/main/java/com/main/feed/model/dto/CommentDto.java
@@ -1,7 +1,10 @@
 package com.main.feed.model.dto;
 
 import com.main.feed.model.entity.Comment;
+import com.main.profile.model.entity.Profile;
+import com.main.profile.model.entity.ProfileImage;
 import com.main.user.model.dto.UserDto;
+import com.main.user.model.entity.User;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,16 +16,22 @@ import java.time.LocalDateTime;
 public class CommentDto {
 
 	private Long commentId;
-	private UserDto userDto;
+	private String userId;
+	private String nickname;
+	private String profileImageUrl;
 	private String content;
 	private LocalDateTime createdDate;
 	// Comment에서는 매칭된 Feed가 무엇인지 알 필요가 없다.
 	
 	public static CommentDto toDto (Comment comment) {
-		
+		User user = comment.getUser();
+		Profile profile = user.getProfile();
+		ProfileImage profileImage = profile.getProfileImage();
 		return new CommentDto(
 				comment.getCommentId(),
-				UserDto.toDto(comment.getUser()),
+				user.getUserId(),
+				user.getNickname(),
+				profileImage.getProfileImageUrl(),
 				comment.getContent(),
 				comment.getCreatedDate()
 		);

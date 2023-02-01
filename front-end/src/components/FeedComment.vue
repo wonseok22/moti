f8bf74d43577c3bb39fcc04bfc2caa9d5968cad2
unknown <sanghwa1103@gmail.com>
2023-02-01 12:02:35 +0900

    피드 댓글창 디자인 및 로직 1차 - 이상화

--- front-end/src/components/FeedComment.vue
+++ front-end/src/components/FeedComment.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>
-    이건 글 상세보기 및 글의 댓글이 표시 되는 페이지입니다.
+  <div class="feed-comment">
+    <MainFeedHeader
+    v-bind:HeaderData="feedHeaderData"/>
+    <SingleFeedBody
+    v-bind:BodyData="feedBodyData"/>
+    <div>
+      <SingleComment 
+      v-for="(item,index) in comments" 
+      v-bind:CommentData="item" 
+      :key="index"/>
+    </div>
+    <div ref="commentInput" class="comment-input">
+      <span class="material-symbols-outlined"
+      style="color:#A3A3A3; font-size:44px">
+          account_circle
+      </span>
+      <form action="">
+        <textarea ref="commentTextarea" name="" cols="35" rows="1"
+        @input="isHeightChanged">
+        </textarea>
+      </form>
+      <button>
+        <p>게시</p>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import MainFeedHeader from '@/components/MainFeedHeader.vue'
+import SingleFeedBody from '@/components/SingleFeedBody.vue'
+import SingleComment from '@/components/SingleComment.vue'
+
 export default {
   name: 'FeedComment',
+  data() {
+    return {
+      feedHeaderData : {
+        missionName: this.$route.query.FeedData.missionName,
+        nickname: this.$route.query.FeedData.nickname,
+        playlistName: this.$route.query.FeedData.playlistName,
+        feedId: this.$route.query.FeedData.feedId,
+      },
+      feedBodyData: {
+        content: this.$route.query.FeedData.content,
+        likes: this.$route.query.FeedData.likes,
+        commentNum: 3,
+        feedId: this.$route.query.FeedData.feedId,
+      },
+      comments: this.$route.query.FeedData.comments,
+    }
+  },
+  created() {
+  },
   methods: {
+    isHeightChanged() {
+      const textarea = this.$refs.commentTextarea
+      textarea.style.height = 'auto'
+      textarea.style.height = textarea.scrollHeight + 'px'
+    }
+  },
+  components: {
+    MainFeedHeader,
+    SingleFeedBody,
+    SingleComment,
   }
 }
 </script>
--- front-end/src/components/SingleFeedBody.vue
+++ front-end/src/components/SingleFeedBody.vue
@@ -2,8 +2,8 @@
   <div class="single-feed">
     <!-- 피드의 글본문에 해당되는 부분 -->
     <div class="feed-text">
-        <p v-if="isThereImg()" v-line-clamp:20="2">{{ text }}</p>
-        <p v-if="isThereImg2()">{{ text }}</p>
+        <p v-show="isThereImg()" v-line-clamp:20="2">{{ BodyData.content }}</p>
+        <p v-show="isThereImg2()">{{ BodyData.content }}</p>
         <button v-if="isThereImg()">
             <p>더보기</p>
         </button>
@@ -14,22 +14,39 @@
     </div>
     <!-- 좋아요와 댓글개수와 관련되는 부분 -->
     <div class="like-comments">
-        <p>좋아요 X개</p>
-        <p>댓글 x개</p>
+        <p>좋아요 {{ BodyData.likes }}개</p>
+        <p>댓글 {{ BodyData.commentNum }}개</p>
     </div>
     <!-- 좋아요 댓글 공유 버튼에 해당되는 부분 -->
     <div class="feed-btns">
         <span class="material-symbols-outlined"
+        v-show="BodyData.likes == false"
         style="color:#A3A3A3;">
             favorite
         </span>
 
-        <span v-on:click="moveToComment" class="material-symbols-outlined"
+        <span class="material-icons-outlined"
+        v-show="BodyData.likes == true"
+        style="color:#FFB100;">
+            favorite
+        </span>
+
+        <span 
+        v-show="this.$route.params.feedId === undefined"
+        v-on:click="moveToComment" 
+        class="material-symbols-outlined"
         style="color:#A3A3A3;">
             mode_comment
         </span>
 
-        <span class="material-symbols-outlined"
+        <span 
+        v-show="this.$route.params.feedId !== undefined"
+        class="material-icons-outlined"
+        style="color:#04C584;">
+            mode_comment
+        </span>
+
+        <span class="material-icons-outlined"
         style="color:#A3A3A3;">
             share
         </span>
@@ -41,6 +58,9 @@
 
 export default {
     name: 'SingleFeed',
+    props: {
+        BodyData: Object,
+    },  
     data() {
         return{
             text: '이건 테스트를 위해 적어놓은 아무말이며, 나는 이것을 위한 노력을 1도 사용하지 않았다. 아무리 그래도 3줄은 넘어야하기 때문에 계속해서 적는것이 좋을거 같다.',
@@ -55,8 +75,18 @@ export default {
             return true
         },
         moveToComment() {
-            this.$router.push({name:'comment', params: {feedId:1}})
-        }
+            this.$axios({
+                method:'get',
+                url:this.$baseUrl + '/feed/1' + '/red'
+            })
+            .then((res) => {
+                const data = res.data.feed
+                this.$router.push({name:'comment', params: {feedId:1}, query:{FeedData: data}})
+            })
+            .catch((error) => {
+                console.log(error.message)
+            })
+        },
     },
 }
 

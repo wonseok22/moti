95601a20b9d7943d4235087f659996b08d28d27d
고명진 <mgin0527@naver.com>
2023-02-02 10:01:38 +0900

    user package code refactor

--- src/main/java/com/main/user/model/service/RedisUtil.java
+++ src/main/java/com/main/user/model/service/RedisUtil.java
@@ -1,9 +1,9 @@
 package com.main.user.model.service;
 
 public interface RedisUtil {
-    public String getData(String key);
-
-    public void setData(String key, String value);
-
-    public void setDataExpire(String key, String value, long duration);
+	public String getData(String key);
+	
+	public void setData(String key, String value);
+	
+	public void setDataExpire(String key, String value, long duration);
 }

b142c645ede4a05c0e915cc92938114b72ea2f53
LEEYEONSOO <noprob1325@naver.com>
2023-02-13 11:21:25 +0900

    Flower 리팩토링(불필요한 코드 제거, 수정, regist -> register 메서드 명 수정)

--- src/main/java/com/main/flower/model/service/FlowerService.java
+++ src/main/java/com/main/flower/model/service/FlowerService.java
@@ -3,9 +3,7 @@ package com.main.flower.model.service;
 import com.main.flower.model.entity.Flower;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.sql.SQLException;
-
 public interface FlowerService {
-	Flower registFlower(MultipartFile image) throws SQLException;
+	Flower registerFlower (MultipartFile image);
 }
 
--- src/main/java/com/main/flower/model/service/FlowerServiceImpl.java
+++ src/main/java/com/main/flower/model/service/FlowerServiceImpl.java
@@ -2,26 +2,14 @@ package com.main.flower.model.service;
 
 import com.main.flower.model.entity.Flower;
 import com.main.flower.model.repository.FlowerRepository;
-import com.main.playlist.model.dto.PlaylistDto;
-import com.main.playlist.model.dto.UserPlaylistDto;
-import com.main.playlist.model.entity.Playlist;
-import com.main.playlist.model.entity.UserPlaylist;
-import com.main.playlist.model.repository.PlaylistRepository;
-import com.main.playlist.model.repository.UserPlaylistRepository;
-import com.main.user.model.entity.User;
-import com.main.user.model.repository.UserRepository;
 import com.main.util.S3Upload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class FlowerServiceImpl implements FlowerService {
+	
 	@Autowired
 	FlowerRepository flowerRepository;
 	
@@ -29,8 +17,7 @@ public class FlowerServiceImpl implements FlowerService {
 	private S3Upload s3Upload;
 	
 	@Override
-	public Flower registFlower(MultipartFile image) throws SQLException {
-		
+	public Flower registerFlower (MultipartFile image) {
 		try {
 			Flower flower = new Flower();
 			String ImagePath = s3Upload.uploadFiles(image, "flowerImages");
